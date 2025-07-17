0e87968efa5b2e341fa12c0c925b4b57218cd10d
Henry Vu <henrynv1@uci.edu>
2025-07-17 11:31:27 -0700

    Some message about the change

--- routes/scanRoutes.js
+++ routes/scanRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import vision from '@google-cloud/vision';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const router = express.Router();
 
 const client = new vision.ImageAnnotatorClient({
-    keyFilename: './vision-key.json',
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
 });
 
 
